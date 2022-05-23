fcd4c7ec10587f8f729d6e23470e488176869e05
JungIn Choi <jungin.choi@navercorp.com>
2022-05-23 20:01:23 +0900

    galaxy data import

--- data-raw/galaxy.R
+++ data-raw/galaxy.R
@@ -1,6 +1,6 @@
 # Velocity of Galaxies -------------------------
 
 galaxy <- read.table("data-raw/galaxy.txt", skip=1)
-colnames(Galaxy) <- c("radius","velocity")
+colnames(galaxy) <- c("radius","velocity")
 
 save(galaxy, file = "data/galaxy.rdata")
